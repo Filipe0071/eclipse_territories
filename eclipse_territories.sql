0f396a3388ce43128afc0f93904f5cbf55cf1e52
Eclipse Modding <75763087+EclipseModding@users.noreply.github.com>
2021-11-29 17:51:12 +0000

    Update eclipse_territories.sql

--- eclipse_territories.sql
+++ eclipse_territories.sql
@@ -3,3 +3,8 @@ CREATE TABLE IF NOT EXISTS `eclipse_territories` (
     `control` varchar(50) NOT NULL DEFAULT '',
     `influence` float NOT NULL DEFAULT 0
 );
+
+INSERT INTO `territories` (`zone`, `control`, `influence`) VALUES
+	('Groove', 'dpls', 100),
+	('ForumDrive', 'dpls', 100),
+	('Rancho', 'dpls', 100);
